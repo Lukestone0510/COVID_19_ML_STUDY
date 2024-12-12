8f9b0157427ae609ca3222d36fd8ce9e5a963578
Lukestone0510 <125825458+Lukestone0510@users.noreply.github.com>
2024-12-11 22:15:41 -0500

    Update main.py

--- main.py
+++ main.py
@@ -1,1 +1,13 @@
+import subprocess
 
+# Run script1.py as a separate process
+subprocess.run(['python', 'src/data/load_data.py'])
+
+# Run script2.py as a separate process
+subprocess.run(['python', 'src/data/preprocess.py'])
+
+subprocess.run(['python', 'src/features/build_features.py'])
+
+subprocess.run(['python', 'src/models/train_model.py'])
+subprocess.run(['python', 'src/models/evaluate_model.py'])
+subprocess.run(['python', 'src/visualization/visualize.py'])
