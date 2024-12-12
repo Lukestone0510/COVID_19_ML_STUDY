a23e23e81fbc1e5b0de91e57d512de36f5528dc7
Lukestone0510 <125825458+Lukestone0510@users.noreply.github.com>
2024-12-11 22:16:37 -0500

    Update main.py

--- main.py
+++ main.py
@@ -1,9 +1,9 @@
 import subprocess
 
-# Run script1.py as a separate process
+
 subprocess.run(['python', 'src/data/load_data.py'])
 
-# Run script2.py as a separate process
+
 subprocess.run(['python', 'src/data/preprocess.py'])
 
 subprocess.run(['python', 'src/features/build_features.py'])
