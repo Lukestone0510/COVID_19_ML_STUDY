0add19595866d7d4fa2dcf6e754d715b95905061
Lukestone0510 <125825458+Lukestone0510@users.noreply.github.com>
2024-12-11 22:17:24 -0500

    Update load_data.py

--- src/data/load_data.py
+++ src/data/load_data.py
@@ -2,6 +2,6 @@ import pandas as pd
 import seaborn as sns
 import matplotlib.pyplot as plt
 
-df = pd.read_csv('CovidDataset.csv')
+df = pd.read_csv('data/raw/CovidDataset.csv')
 df = df.replace({'Yes': 1, 'No': 0})
 df
